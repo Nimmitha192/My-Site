7821a83c82c28b222d78f9e1d97ba4b7822ff798
Nimmitha Idirimana <nimmitha192@gmail.com>
2026-02-03 18:29:25 +0530

    completed site

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
@@ -10,3 +11,44 @@ menuOpenButton.addEventListener("click", () => {
 menuCloseButton.addEventListener("click", () => menuOpenButton.click()
     //Close the menu
 );
+
+//close menu when the nav link is clicked
+
+navLinks.forEach(link => {
+    link.addEventListener("click" , () => menuOpenButton.click());
+});
+
+
+//Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween:25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //responsive breackpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    600: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+
+});
